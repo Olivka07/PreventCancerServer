92c329b4140d92732679075a16f03c9c4383cff3
Olivka07 <uchenik.ikt@yandex.ru>
2023-06-04 15:00:00 +0400

    new url cors

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const cors = require('cors')
 const app = express()
 
 app.use(cors({
-    origin: ["https://prevent-cancer-client.vercel.app/"]
+    origin: ["https://prevent-cancer-client.vercel.app"]
 }))
 
 app.use(express.json({extended:true}))
